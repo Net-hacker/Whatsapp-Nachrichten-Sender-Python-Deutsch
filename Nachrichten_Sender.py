8c623d4d727d8e4eb4925a7fa57a17b8ba4038cc
Juri Zockt <85522443+Juri-Zockt@users.noreply.github.com>
2021-06-12 22:55:45 +0200

    Add files via upload

---
+++ Nachrichten_Sender.py
@@ -0,0 +1,25 @@
+#Import
+import pywhatkit
+from pynput.keyboard import Key, Controller
+import time
+import os
+
+#Das die .txt File gelöscht wird
+if os.path.exists("/Users/Juri Zockt/Documents/Codes/pywhatkit_dbs.txt"):
+    os.remove("/Users/Juri Zockt/Documents/Codes/pywhatkit_dbs.txt")
+
+#Keyboard Variabel
+keyboard = Controller()
+
+#Das eigentliche Script
+print('Hast du dein Handy schon mit Whatsapp Web verbunden. Wenn Nein drück Strg + c')
+time.sleep(5)
+telephone = int(input('Bitte Nummer eingeben (Ohne Vorwahl und Leerzeichen): '))
+message = str(input('Die Nachricht: '))
+h = int(input('Zu welcher Stunde soll die Nachricht ankommen: '))
+m = int(input('Zu welcher Minute soll die Nachricht ankommen: '))
+pywhatkit.sendwhatmsg('+49 ' + str(telephone), message, h, m)
+keyboard.press(Key.esc)
+keyboard.release(Key.esc)
+keyboard.press(Key.enter)
+keyboard.release(Key.enter)
